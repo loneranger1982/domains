7435594d42a66205318781e840f5597ca1e0dbef
Martin Porter <Admin@shenobis.co.uk>
2017-10-18 20:37:38 +0000

    heroku error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   
   def home
     @domain=Domain.count
-    @filtered=Domain.where('haswebsite IS NOT NULL').count
-    @websites=Domain.where('haswebsite =?',true).count
+    @filtered=Domain.where('domains.haswebsite IS NOT NULL').count
+    @websites=Domain.where('domains.haswebsite =?',true).count
   end
 end
