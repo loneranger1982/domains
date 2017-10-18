786267ccb8c5ab0cc43fded5c42e7ac3aa72dc28
Martin Porter <Admin@shenobis.co.uk>
2017-10-18 20:50:39 +0000

    DashBoard Typo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   
   def home
     @domain=Domain.count
-    @filtered=Domain.where('"domains.hasWebsite" IS NOT NULL').count
-    @websites=Domain.where('"domains.hasWebsite" =?',true).count
+    @filtered=""#Domain.where('"domains.hasWebsite" IS NOT NULL').count
+    @websites=""#Domain.where('"domains.hasWebsite" =?',true).count
   end
 end
