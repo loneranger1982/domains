4b0b63b2e4898d96da463e20bef3cf210d7a110b
Martin Porter <Admin@shenobis.co.uk>
2017-11-07 17:01:21 +0000

    add domain end filtering

--- app/datatables/domain_datatable.rb
+++ app/datatables/domain_datatable.rb
@@ -49,7 +49,7 @@ private
       search = params["columns"]["8"]["search"]["value"]
       products = products.where(haswebsite: search)
     end
-    if params["columns"]["8"]["search"]["value"].length > 0
+    if params["columns"]["1"]["search"]["value"].length > 0
       search = params["columns"]["8"]["search"]["value"]
       products = products.where("domainname like :search",search: "%" + svalue)
     end
