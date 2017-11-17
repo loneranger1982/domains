a96a0bb090d5fd5649876b7f829381046d0c3c78
Martin Porter <admin@shenobis.co.uk>
2017-11-17 11:05:21 +0000

    bulk import

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.3.4'
 
-gem 'bulk_insert'
+gem 'activerecord-import'
 gem 'sidekiq'
 gem 'sidekiq-status'
 gem 'curb'
--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -21,53 +21,59 @@ class HardWorker
       end
       
     end
-    totalsize=File.size(Dir.pwd + "/expiring_service_auctions.xml")
-    total 450000
-    parser = Saxerator.parser(File.new(Dir.pwd + "/expiring_service_auctions.xml"))
-    Domain.bulk_insert(:domainname, :link, :auctionendtime, :numberOfBids, :domainAge, :traffic,:source,:html,ignore: true) do |worker|
-      worker.set_size=10
-      parser.for_tag(:item).each do |item|
-      
     
-        s=item['description']
-        details=s.split(',')
-        hash={}
-        details.each do |e|
-          kv=e.split(':',2)
-          hash[kv[0].to_s.strip]=kv[1].to_s.strip
-        end
+    total 450000
+    message =0
+    parser = Saxerator.parser(File.new(Dir.pwd + "/expiring_service_auctions.xml"))
+    
+    domains=[]
+    parser.for_tag(:item).each do |item|
+      
+    timestart=Time.now.to_i
+      s=item['description']
+      details=s.split(',')
+      hash={}
+      details.each do |e|
+        kv=e.split(':',2)
+        hash[kv[0].to_s.strip]=kv[1].to_s.strip
+      end
      
-        if Time.strptime(hash['Auction End Time'],"%m/%d/%Y %I:%M %p (%Z)").to_i < Time.now.to_i
+      if Time.strptime(hash['Auction End Time'],"%m/%d/%Y %I:%M %p (%Z)").to_i < Time.now.to_i
           
-          next
-        end
+        next
+      end
 
-        html=loadhtml(item['title'])
-        endtime=Time.strptime(hash['Auction End Time'],"%m/%d/%Y %I:%M %p (%Z)").to_i
-        worker.add domainname: item['title'], link: item['link'], auctionendtime: endtime, domainAge: hash['domainAge'].to_i, traffic: hash['Traffic'].to_i, source:"GoDaddy Auctions",html:html
-      #domains=Domain.new
-      #domains.domainname=item['title']
-      #domains.link=item['link']
-      #domains.auctionType=hash['Auction Type']
-      #domains.auctionendtime=Time.strptime(hash['Auction End Time'],"%m/%d/%Y %I:%M %p (%Z)").to_i
-      #domains.price=hash['Price']
-      #domains.numberOfBids=hash['Number of Bids']
-      #domains.domainAge=hash['Domain Age'].to_i
-      #domains.valuation=hash['Valuation']
-      #domains.traffic=hash['Traffic'].to_i
-      #domains.isAdult=hash['IsAdult']=='true'? true: false
-      #domains.source="GoDaddy Auctions"
-     
+      html=loadhtml(item['title'])
+        
+
+        
+      d=Domain.new
+      d.domainname=item['title']
+      d.link=item['link']
+      d.auctionType=hash['Auction Type']
+      d.auctionendtime=Time.strptime(hash['Auction End Time'],"%m/%d/%Y %I:%M %p (%Z)").to_i
+      d.price=hash['Price']
+      d.numberOfBids=hash['Number of Bids']
+      d.domainAge=hash['Domain Age'].to_i
+      d.valuation=hash['Valuation']
+      d.traffic=hash['Traffic'].to_i
+      d.isAdult=hash['IsAdult']=='true'? true: false
+      d.source="GoDaddy Auctions"
+      d.html=html
+      domains << d
       #domains.save
       #ParsedomainsWorker.perform_async(domains.id)
         i=i+1
-        at  i
-      
-      
-    
+        at  i,message
+      if i % 100 == 0
+        Domain.import domains, on_duplicate_key_ignore: true
+        domains=[]
       end
-
+      timeend=Time.now.to_i
+      message=((timeend-timestart) * 1000 * (450000-i))
     end
+    Domain.import domains, on_duplicate_key_ignore: true
+    domains=[]
   end
 
   def loadhtml(url)
@@ -87,34 +93,34 @@ class HardWorker
         ##@@html = HTTParty.get("http://www." + domain.domainname,follow_redirects: true)
         #puts page
       rescue Curl::Err::RecvError
-        
+          html="Error"
         return
       rescue Net::OpenTimeout
-        
+        html="Error"
         return
       rescue Curl::Err::TooManyRedirectsError
-        
+        html="Error"
         return
       rescue Curl::Err::HostResolutionError
-       
+       html="Error"
         return
       rescue Curl::Err::TimeoutError
-        
+        html="Error"
         return
       rescue Curl::Err::SSLPeerCertificateError
-        
+        html="Error"
         return
       rescue Curl::Err::ConnectionFailedError
-        
+        html="Error"
         return
         
       rescue Net::ReadTimeout => error
         if times_retried < max_retries
           times_retried += 1
-          
+          html="Error"
           retry
         else
-          
+          html="Error"
           return
         end
       end
