d1b6be2a61e8f04e5c421ab528364dd15d6c509a
root <root@ns3269914.ip-37-59-56.eu>
2017-11-30 13:50:05 +0100

    php parser

--- scrape.php
+++ scrape.php
@@ -1,28 +1,44 @@
 <?php
+
+ini_set('memory_limit','16M');
+set_time_limit(800);
 class scrape {
-$conn=""
-function scrapeWebsite(){
+var $conn="";
+function scrapeWebsite($offset){
 	include 'simple_html_dom.php';
 $host="localhost";
 $username="root";
 $password="BrookePorter1281";
 $db="domains";
 $this->conn=new mysqli($host,$username,$password,$db);
+echo $offset;
+$sql="select * from domains where scraped is NULL LIMIT $offset,100";
 
-$sql="select * from domains where scraped is NULL LIMIT 100";
-
-$result=$conn->query($sql);
+$result=$this->conn->query($sql);
 
 if($result->num_rows > 0){
-
+	
 	while($row=$result->fetch_assoc()){
-		
-		$html=getHTML("http://www." . $row['domainname']);
-		$find=runFilters($html);
+		$sqlUpdate="update domains set scraped=true WHERE id=" .$row['id'];
+                $this->conn->query($sqlUpdate);
+
+		echo $row['domainname'];
+		$html=$this->getHTML("http://www." . $row['domainname']);
+		if(strlen($html)<10){
+			$sqlUpdate="update domains set html='$html',haswebsite=false,filter='BLANK RETURNED',scraped=true WHERE id=" .$row['id'];
+			$this->conn->query($sqlUpdate);
+			continue;	
+		}
+		if($this->containsWord($html,"The resource you are looking for has been removed")){
+		 	$sqlUpdate="update domains set html='$html',haswebsite=false,filter='REMOVED',scraped=true WHERE id=" .$row['id'];
+                        $this->conn->query($sqlUpdate);
+                        continue;
+		}
+		$find=$this->runFilters($html);
 		if($find !=false){
-			$sqlUpdate="update domains set haswebsite=false,filter='$find',scraped=true WHERE id=" .$row['id'];
+			$sqlUpdate="update domains set html='$html',haswebsite=false,filter='$find',scraped=true WHERE id=" .$row['id'];
 		}else{
-			$sqlUpdate="update domains set haswebsite=true,filter='$find',scraped=true WHERE id=" .$row['id'];
+			$sqlUpdate="update domains set html='$html',haswebsite=true,filter='$find',scraped=true WHERE id=" .$row['id'];
 		}
 		
 		
@@ -40,6 +56,8 @@ function getHTML($domain){
 	$ch = curl_init();
 	curl_setopt($ch,CURLOPT_URL,$domain);
 	curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,30);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 	curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false); 
 	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);  
@@ -49,7 +67,10 @@ function getHTML($domain){
 
 	return $result;
 }
-
+function containsWord($str, $word)
+{
+    return !!preg_match('#\\b' . preg_quote($word, '#') . '\\b#i', $str);
+}
 function runFilters($html){
 	$htmlDom=new simple_html_dom();
 
@@ -58,18 +79,21 @@ function runFilters($html){
 	$filtersSQL="select * from filters";
 	$result=$this->conn->query($filtersSQL);
 	while($row=$result->fetch_assoc()){
-		$items=$html->find($row['selector']);
+		$items=$htmlDom->find($row['selector']);
 		foreach($items as $find){
 			switch ($row['attr']){
 				case "text":
-					if($find->outertext == $row['regex']){
+					
+					if($this->containsWord($find->innertext,$row['regex'])){
+					
 					return $row['name'];
 			
 					}
 				break;
 
 				case "src":
-					if($find->src == $row['regex']){
+					if($this->containsWord($find->src,$row['regex'])){
+					
 					return $row['name'];
 			
 					}
@@ -81,4 +105,7 @@ function runFilters($html){
 	return false;
 }
 }
+echo $argv[1];
+$a=new scrape();
+$a->scrapeWebsite($argv[1]);
 ?>
