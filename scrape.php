c40e83c143a57001d385c3017461e1a46c6c42b9
Martin Porter <admin@shenobis.co.uk>
2018-01-21 17:21:27 +0000

    upgrate curl

--- scrape.php
+++ scrape.php
@@ -52,25 +52,49 @@ mysqli_close($this->conn);
 }
 
 
-function getHTML($domain){
-	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL,$domain);
-	curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,30);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Connection: Keep-Alive',
-    'Keep-Alive: 300'
-));
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-	curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false); 
-	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);  
-	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-	$result=curl_exec($ch);
-	curl_close($ch);
+function getHTML($url, $javascript_loop = 0, $timeout = 5) {
+		$url = str_replace("&amp;", "&", urldecode(trim($url)));
+		//echo "URL : "+$url;
+		$cookie = tempnam("/tmp", "CURLCOOKIE");
+		$ch     = curl_init();
+		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
 
-	return $result;
-}
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_ENCODING, "");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);# required for https urls
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+		$content  = curl_exec($ch);
+		$response = curl_getinfo($ch);
+		curl_close($ch);
+
+		if ($response['http_code'] == 301 || $response['http_code'] == 302) {
+			ini_set("user_agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+
+			//print_r($response);
+			if ($headers = get_headers($response['url'])) {
+				foreach ($headers as $value) {
+					if (substr(strtolower($value), 0, 9) == "location:") {
+						return $this->get_url(trim(substr($value, 9, strlen($value))));
+					}
+				}
+			}
+		}
+
+		if ((preg_match("/>[[:space:]]+window\.location\.replace\('(.*)'\)/i", $content, $value) || preg_match("/>[[:space:]]+window\.location\=\"(.*)\"/i", $content, $value)) &&
+			$javascript_loop < 5
+		) {
+			return $this->get_url($value[1], $javascript_loop+1);
+		} else {
+
+			return $content;
+		}
+	}
 function containsWord($str, $word)
 {
     return !!preg_match('#\\b' . preg_quote($word, '#') . '\\b#i', $str);
